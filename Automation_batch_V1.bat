dacc2cf313026f746de1b4e601a36f9fcc79fcbf
Swapnil Sanjay Chavan <ssc.swapnil.mobile@gmail.com>
2022-08-24 18:34:59 +0530

    Add files via upload

---
+++ Automation_batch_V1.bat
@@ -0,0 +1,4 @@
+d:                 :: Drive of folder script 
+cd D:\test         :: Change directory to script folder 
+python test.py     :: Run python script with default Python  
+pause			   :: Pause to see the output of the scripts
---
+++ test.py
@@ -0,0 +1,7 @@
+def python_batch():
+    """_summary_: Test function to test the batch files.
+    """
+    print("Thank you Swapnil, We learn to create batch file to schedule python script...")
+   
+if __name__ == "__main__":
+    test = python_batch()
